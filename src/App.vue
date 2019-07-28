9332831a7932f0c2f236675d6670ac1e697cd938
joshua-porter-dev <jporter4117@gmail.com>
2019-07-28 12:49:19 -0500

    added toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <v-app>
+    <v-app-bar color='red darken-3'  dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Page Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
     
     <v-content>
       <router-view></router-view>
